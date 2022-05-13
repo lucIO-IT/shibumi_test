33439fb99e56f54008559bf15d0a4660af6df9c6
emanueleromagnoli <emanuele@co-brains.com>
2022-05-13 22:58:54 +0200

    [shibumi_test]setup todo list services

---
+++ core/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('todos/', views.TodoListAPIView.as_view())
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import IsAuthenticated
+
+from .config.serializers import TodoSerializer
+from .models import Todo
 
 # Create your views here.
+
+
+class TodoListAPIView(ListCreateAPIView):
+
+    model = Todo
+    serializer_class = TodoSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        return self.model.objects.filter(owner=self.request.user)
