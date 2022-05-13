926d1d1d0dc18e06a9aa3cc75cbd6889e6b3e797
emanueleromagnoli <emanuele@co-brains.com>
2022-05-13 21:12:40 +0200

    [shibumi_test]setup Todo model in core app

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Todo(models.Model):
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='todos')
+    name = models.CharField(max_length=250, blank=False, null=False)
+    image = models.ImageField(blank=False, null=False) # use base64 upload
+    deadline = models.DateTimeField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
